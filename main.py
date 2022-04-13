5f865f70bc589bd9894f3e5e58629024506796a1
Anton <antel460@student.liu.se>
2022-04-13 21:36:14 +0200

    25B - responds and prints message in terminal now

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import websockets
 
 async def echo(websocket):
     async for message in websocket:
+        print(message)
         await websocket.send(message)
 
 async def main():
     print("Started")
-    async with websockets.serve(echo, 6000):
+    async with websockets.serve(echo, None, 6000):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
