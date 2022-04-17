1718f4f661ead2048b0cd327effacbee5132623d
Anton <antel460@student.liu.se>
2022-04-17 16:55:08 +0200

    25B: Switched back to frombuffer with extra arguments

--- client_side_test_programs/client.py
+++ client_side_test_programs/client.py
@@ -11,6 +11,6 @@ async def send_data(data):
 
 f = "4_ref.mp3"
 sound, sampling_rate = librosa.load(f)
-sound_16khz = librosa.resample(sound, orig_sr=sampling_rate, target_sr=16000)
+sound_16khz = librosa.resample(sound, orig_sr=sampling_rate, target_sr=16_000)
 #np_file = np.load(sound_16khz, allow_pickle=True)
-asyncio.run(send_data(sound_16khz.tostring()))
+asyncio.run(send_data(sound_16khz.tobytes()))
