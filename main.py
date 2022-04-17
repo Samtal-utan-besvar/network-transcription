f1e8d312940a9bd6317d75a914f4de483e2496fd
Anton <antel460@student.liu.se>
2022-04-17 15:15:12 +0200

    25B: converts numpy array into strings and back for websocket sendability

--- main.py
+++ main.py
@@ -2,22 +2,36 @@ import asyncio
 import websockets
 import threading
 from transcription_process import transcribe
+import numpy as np
 
 messages = []
 
+
+"""
+Starts several processes of parallell transcription instances and delegates incoming transcription work 
+amongst theese from the work queue (messages). 
+"""
 def request_handler():
     #number_of_procecess = 1
     while len(messages)==0:
         pass
     message = messages.pop(0)
-    transcribe(message)
+    transcribe(np.fromstring(message))
 
 
+"""
+Websocket answering function. Adds all incoming text into a work queue (messages)
+"""
 async def echo(websocket):
     async for message in websocket:
         messages.append(message)
         await websocket.send(message)
 
+
+"""
+Main function, sets up the listening websocket and handles all incoming ws work requests.
+Starts thread in request handler to run in parallell. 
+"""
 async def main():
     print("Started")
     req_hand = threading.Thread(target=request_handler, args=())
